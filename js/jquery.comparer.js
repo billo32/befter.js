b1ee2280bd2af5a73d615f483a69139e00956842
Dmitry Bilichenko <bilichenkodmitry@gmail.com>
2014-04-12 14:30:58 +0400

    v 0.1 (alpha)
    Alpha версия

---
+++ js/jquery.comparer.js
@@ -0,0 +1,37 @@
+(function($) { 
+
+	var defaults = { 
+		position: '50px', 
+		bold: '1px',
+		style: 'solid',
+		color: 'red',
+	};
+
+	var options;
+
+	var methods = {	
+		init:function(params) {	
+			var options = $.extend({}, defaults, params);
+			$.each($(this),function(){
+				$this = $(this);		        
+				$this.find('img').first().wrap('<div class="old" style="position: absolute; float: left;">');
+				$this.find('img').last().wrap('<div class="new" style="position: absolute; float: left; overflow: hidden; border-right: ' + options.bold + ' '+ options.style +' ' + options.color + '; ">');	
+				$this.find('img').last().parent().css('width', options.position);
+			})
+			return this.mousemove(function(e){
+				$(this).find('img').last().parent().css('width', e.offsetX);
+			});
+		}
+	};
+
+	$.fn.comparer = function(method){
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Метод "' +  method + '" не найден в плагине jQuery.comparer' );
+		}
+
+	};
+})(jQuery);
